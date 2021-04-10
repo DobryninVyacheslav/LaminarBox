5bf66a1983376ec5ec5e87e659c453d5b398eba4
SlavaDobr <slavad999@gmail.com>
2021-04-10 20:42:26 +0300

    refactoring

--- src/process_recognition/process_recognition.py
+++ src/process_recognition/process_recognition.py
@@ -9,6 +9,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 import tensorflowjs as tfjs
+import utils.tf_utils
 
 # Download and explore the dataset
 data_dir = pathlib.Path(
@@ -102,29 +103,7 @@ history = model.fit(
     epochs=epochs
 )
 
-# Visualize training results
-# acc = history.history['accuracy']
-# val_acc = history.history['val_accuracy']
-#
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
-#
-# epochs_range = range(epochs)
-#
-# plt.figure(figsize=(8, 8))
-# plt.subplot(1, 2, 1)
-# plt.plot(epochs_range, acc, label='Training Accuracy')
-# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-# plt.legend(loc='lower right')
-# plt.title('Training and Validation Accuracy')
-#
-# plt.subplot(1, 2, 2)
-# plt.plot(epochs_range, loss, label='Training Loss')
-# plt.plot(epochs_range, val_loss, label='Validation Loss')
-# plt.legend(loc='upper right')
-# plt.title('Training and Validation Loss')
-# plt.show()
-
+utils.tf_utils.show_training_results(history, epochs)
 # Predict on new data
 
 check_photo = val_ds.take(1)
