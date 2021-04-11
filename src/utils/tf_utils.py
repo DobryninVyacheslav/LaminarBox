34a926dd75976be13b0b2df63a10cc2754da0da2
SlavaDobr <slavad999@gmail.com>
2021-04-11 08:22:15 +0300

    show_images with as_numpy_array

--- src/utils/tf_utils.py
+++ src/utils/tf_utils.py
@@ -17,19 +17,26 @@ def print_size(ds):
     print("Size: ", len(get_images(ds)))
 
 
-def show_images(ds, subplot_number=9, subplot_x_num=3,
-                subplot_y_num=3, with_class_name=False, with_numpy_as_type=True):
+def show_images(ds, subplot_number=9, subplot_x_num=3, subplot_y_num=3,
+                with_class_name=False, with_numpy_as_type=True, as_numpy_array=False):
     plt.figure(figsize=(10, 10))
-    for images, labels in ds.take(1):
+
+    if as_numpy_array:
         for i in range(subplot_number):
-            ax = plt.subplot(subplot_x_num, subplot_y_num, i + 1)
-            if with_numpy_as_type:
-                plt.imshow(images[i].numpy().astype("uint8"))
-            else:
-                plt.imshow(images[i])
-            if with_class_name:
-                plt.title(ds.class_names[labels[i]])
+            ax = plt.subplot(subplot_y_num, subplot_x_num, i + 1)
+            plt.imshow(ds[i])
             plt.axis("off")
+    else:
+        for images, labels in ds.take(1):
+            for i in range(subplot_number):
+                ax = plt.subplot(subplot_y_num, subplot_x_num, i + 1)
+                if with_numpy_as_type:
+                    plt.imshow(images[i].numpy().astype("uint8"))
+                else:
+                    plt.imshow(images[i])
+                if with_class_name:
+                    plt.title(ds.class_names[labels[i]])
+                plt.axis("off")
     plt.show()
 
 
