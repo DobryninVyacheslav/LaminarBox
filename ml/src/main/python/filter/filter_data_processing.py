e60a013ba525070cbeb4c152bb51be17513ed4d0
SlavaDobr <slavad999@gmail.com>
2021-04-23 14:23:51 +0300

    refactoring

--- ml/src/main/python/filter/filter_data_processing.py
+++ ml/src/main/python/filter/filter_data_processing.py
@@ -31,6 +31,7 @@ train_labels = train_dataset.pop('time')
 test_labels = test_dataset.pop('time')
 
 
+# Normalize data
 def norm(x):
     return (x - train_stats['mean']) / train_stats['std']
 
@@ -40,18 +41,16 @@ normed_test_data = norm(test_dataset)
 
 
 def build_model():
-    model = keras.Sequential([
+    dnn_model = keras.Sequential([
         layers.Dense(64, activation='relu', input_shape=[len(train_dataset.keys())]),
         layers.Dense(64, activation='relu'),
         layers.Dense(1)
     ])
 
-    optimizer = tf.keras.optimizers.RMSprop(0.001)
-
-    model.compile(loss='mse',
-                  optimizer=optimizer,
-                  metrics=['mae', 'mse'])
-    return model
+    dnn_model.compile(loss='mse',
+                        optimizer=tf.keras.optimizers.RMSprop(0.001),
+                        metrics=['mae', 'mse'])
+    return dnn_model
 
 
 model = build_model()
